91074efc6901c967d7ff775d90f21a14505a9714
Abadi-Dev <65283737+Abadi-Dev@users.noreply.github.com>
2022-04-01 21:32:15 +0300

    basic code refactor, still some work to do.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const _ = require("lodash");
+const app = express();
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -12,13 +12,10 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
-
 // array to have the posts that we will display on the home page.
 let posts = [];
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -27,7 +24,6 @@ app.get("/", function (req, res) {
     homeStartingContent: homeStartingContent,
     posts: posts,
   });
-  //console.log(posts);
 });
 
 app.get("/about", function (req, res) {
@@ -38,18 +34,18 @@ app.get("/contact", function (req, res) {
   res.render("contact.ejs", { contactContent: contactContent });
 });
 
-app.get("/compose", function (req, res) {
-  res.render("compose.ejs");
-});
-
-app.post("/compose", function (req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postContent,
-  };
-  posts.push(post);
-  res.redirect("/");
-});
+app
+  .get("/compose", function (req, res) {
+    res.render("compose.ejs");
+  })
+  .post("/compose", function (req, res) {
+    const post = {
+      title: req.body.postTitle,
+      content: req.body.postContent,
+    };
+    posts.push(post);
+    res.redirect("/");
+  });
 
 //FIXME: fix the issue with some parameters, for example if i compose a post
 //with the titel of "TesT", and try to acces the route with "tESt" it will not work
